5745bdbe8d885678db2190e681d9ef8e80dbbaa6
w.ryan.jung <w.ryan.jung@gmail.com>
2022-03-01 15:52:40 +0000

    create model

---
+++ MusicLibraryDemo/MusicLibrary/Models/Track.cs
@@ -0,0 +1,10 @@
+namespace MusicLibrary.Models;
+
+public class Track
+{
+    public string Artist { get; set; }
+    public string Title { get; set; }
+    public string Duration { get; set; }
+    public string IssuedDate { get; set; }
+    public string Genre { get; set; }
+}
