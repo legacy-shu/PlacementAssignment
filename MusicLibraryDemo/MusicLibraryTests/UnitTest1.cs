95d67b83b055db026d1cd6ac031cd3d3c74ba424
w.ryan.jung <w.ryan.jung@gmail.com>
2022-03-01 16:20:59 +0000

    added test case code

--- MusicLibraryDemo/MusicLibraryTests/UnitTest1.cs
+++ MusicLibraryDemo/MusicLibraryTests/UnitTest1.cs
@@ -1,11 +1,42 @@
+using System.Linq;
 using Xunit;
-
-namespace MusicLibraryTests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void GetMusicTracksReturnsAtleastOnetrackOrMore()
     {
+        var db = new DB("./musiclibrary.db");
+        db.CreateTable();
+        db.InsertValue();
+
+        var musicLibrary = new MusicLibrary.Models.MusicLibrary();
+        var tracks = musicLibrary.GetMusicTracks();
+        Assert.True(tracks.Count() > 0);
+    }
+    
+    [Fact]
+    public void GetMusicTracksReturnsTracksWithMatchingGenre()
+    {
+        var db = new DB("./musiclibrary.db");
+        db.CreateTable();
+        db.InsertValue();
+
+        var musicLibrary = new MusicLibrary.Models.MusicLibrary();
+        var tracks = musicLibrary.GetMusicTracks("Pop");
+        Assert.True(tracks.Count() == 3);
+    }
+    
+    [Fact]
+    public void GetMusicTracksReturnsNoTracksWithZeroDuration()
+    {
+        var db = new DB("./musiclibrary.db");
+        db.CreateTable();
+        db.InsertValue();
+
+        var musicLibrary = new MusicLibrary.Models.MusicLibrary();
+        var tracks = musicLibrary.GetMusicTracks();
+        var isIncludeZeroDuration = tracks.Any(r => r.Duration == "0:00");
+        Assert.True(isIncludeZeroDuration == false);
     }
 }
